89ab3b9d32074e4ed0e8404827ddbee1307671d6
Aditya Gupta <114931868+codecommander03@users.noreply.github.com>
2023-09-29 20:59:03 +0530

    Create 0896.Monotonic Array.cpp
    https://leetcode.com/problems/monotonic-array/

---
+++ 0896.Monotonic Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        bool inc = true, dec = true;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i-1]>nums[i]) inc = false;
+            if(nums[i-1]<nums[i]) dec = false;
+            if((inc|dec)==false) return false;
+        }
+        return true;
+    }
+};
